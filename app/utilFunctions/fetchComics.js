4d26565e955559bd901c8820cccad64e7feabcf8
Grzegorz Stochel <danielstochel332@gmail.com>
2019-12-06 18:26:46 +0100

    create a loop which grab last eight comics

--- app/utilFunctions/fetchComics.js
+++ app/utilFunctions/fetchComics.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
-const fetchComics = async (comics, index) => {
-  const fetchComics = await axios.get(
-    `https://cors-anywhere.herokuapp.com/https://xkcd.com/${index}/info.0.json`
-  );
+const fetchComics = async (index) => {
+  const comics = []
 
-  comics.push(fetchComics.data);
+  for (let i = index; i > index - 8; i-- ) {
+    const fetchComics = await axios.get(
+      `https://cors-anywhere.herokuapp.com/https://xkcd.com/${i}/info.0.json`
+    );
+    
+    comics.push(fetchComics.data);
+  }
+  
+  return comics
 };
 
 export default fetchComics;
