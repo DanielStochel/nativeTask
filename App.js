bcdc888a61b6b7d90ccaf0c7914c2e92b5f464c5
Grzegorz Stochel <danielstochel332@gmail.com>
2019-12-06 16:38:59 +0100

    add react navigation details

--- App.js
+++ App.js
@@ -1,38 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import fetchComics from "./app/utilFunctions/fetchComics";
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Homepage from "./app/components/Homepage";
+import DetailsComp from "./app/components/DetailsComp";
+
+const RootStack = createStackNavigator(
+  {
+    Home: Homepage,
+    Details: DetailsComp
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
 
 const App = props => {
-  const [comics, setComics] = useState([]);
-
-  useEffect(async () => {
-    const lastEightComics = [];
-    await fetchComics(lastEightComics, 2237);
-    await fetchComics(lastEightComics, 2236);
-    await fetchComics(lastEightComics, 2235);
-    await fetchComics(lastEightComics, 2234);
-    await fetchComics(lastEightComics, 2233);
-    await fetchComics(lastEightComics, 2232);
-    await fetchComics(lastEightComics, 2231);
-    await fetchComics(lastEightComics, 2230);
-    setComics(lastEightComics);
-  }, []);
   
-  const renderComics = () => {
-    return comics.map(detailOfComics => {
-      return (
-        <View>
-          <Text>{detailOfComics.alt}</Text>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: detailOfComics.img }}
-          />
-        </View>
-      );
-    });
-  };
-
-  return <View>{renderComics()}</View>;
+  return <AppContainer />
 };
 
 export default App;
